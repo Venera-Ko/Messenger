264b0b74f69c6b069b587f91418f2db2cbad0f0f
V-theG <bmboxx@icloud.com>
2022-10-05 10:54:10 +0300

    initial commit

---
+++ Controllers/ConversationsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by V K on 05.10.2022.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "loggedIn")
+        if !isLoggedIn {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
+    }
+
+}
+
---
+++ Resourses/Extensions.swift
@@ -0,0 +1,36 @@
+//
+//  Extensions.swift
+//  Messenger
+//
+//  Created by V K on 05.10.2022.
+//
+
+import UIKit
+
+extension UIView {
+    
+    public var width: CGFloat {
+        self.frame.size.width
+    }
+    
+    public var height: CGFloat {
+        self.frame.size.height
+    }
+    
+    public var top: CGFloat {
+        self.frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        self.frame.size.height + self.frame.origin.y
+    }
+
+    public var left: CGFloat {
+        self.frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        self.frame.size.width + self.frame.origin.x
+    }
+    
+}
